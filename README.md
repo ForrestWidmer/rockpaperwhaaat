255046afd258c200da3ee0fe2ef80c3519d5263a
ForrestWidmer <forrest.widmer@gmail.com>
2014-05-20 19:31:14 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rock, Paper, Scissors game!
+
+1. Select a sign to throw at your computer! (Just dont actually throw anything....)
+2. Hit the "Play!" button to play to start the game.
+3. You will then be shown the results! Win, Lose, or Tie.
+4. Want revenge? Hit the "I want revenge!" button.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rockpaperwhaaat::Application.config.secret_token = 'd702e330adc47cfd1a5ec5aaa39c365113a87104d8ecda7d2b09e767d513862739d527958d3fc3cffa0f84e03a903bb78f0dfcb60474d31d0d98091dc45f0d99'
